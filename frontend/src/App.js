bfa757e1251963092279b9403e2dc214d493d55b
eymmProjects <64175791+eymmProjects@users.noreply.github.com>
2023-07-02 22:10:24 +0800

    frontend-backend

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import { LiveVideo } from "./svg";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello Frontend</h1>
+      <div className="all_friends_icon" />
+      <div className="all_friends_icon" />
+      <i className="trash_icon" />
+      <LiveVideo color="red"/>
+    </div>
+  );
+}
+
+export default App;
